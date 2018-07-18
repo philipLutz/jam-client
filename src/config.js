0ee217dd0ba54a6caaad9e1c278f5f09350e2939
Philip Lutz <philiplutz413@gmail.com>
2018-07-17 19:11:13 -0700

    ughh come on cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,1 @@
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "https://quiet-brushlands-87414.herokuapp.com/api"
-};
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
