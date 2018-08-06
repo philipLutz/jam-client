4416335a1a0688746c755369773479a38abf699a
Philip Lutz <philiplutz413@gmail.com>
2018-08-06 14:14:32 -0700

    finished onboarding pages styling

--- src/components/onboarding/AboutSection.js
+++ src/components/onboarding/AboutSection.js
@@ -7,10 +7,10 @@ export default class AboutSection extends React.Component {
 		return (
 			<ScrollableAnchor id={'about'}>
 				<section className="about-section">
-					<h2 className="title">About</h2>
-					<div className="about-content">
+					<h2 className="title-about">About</h2>
+					<p className="about-content">
 						Looking to play some music?  Jam is the simplest way to connect with other musicians and find when and where the jam sessions are happening.  Just like a bulletin board at your local cafe, Jam is easy to use and conveniently lets you know what's going on.  
-					</div>
+					</p>
 					<div className="button-container">
 						<a href='#top' className="top-button">Scroll to Top</a>
 					</div>
