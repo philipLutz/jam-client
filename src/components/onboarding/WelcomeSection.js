268ba0e5d94cdf035fddb9f8a2cb8055b5d304d8
Philip Lutz <philiplutz413@gmail.com>
2018-08-14 01:57:17 -0700

    styling board and account

--- src/components/onboarding/WelcomeSection.js
+++ src/components/onboarding/WelcomeSection.js
@@ -22,7 +22,7 @@ export class WelcomeSection extends React.Component {
 		}
 		return (
 			<section className="welcome">
-				<img src="https://corandemphotography.files.wordpress.com/2011/01/punchbrothers121210_65.jpg" />
+				<img src="https://corandemphotography.files.wordpress.com/2011/01/punchbrothers121210_65.jpg" alt="folk jam" />
 				<div className="title-container">
 					<h1 className="title">Jam</h1>
 					<h3 className="dialog">{dialog}</h3>
